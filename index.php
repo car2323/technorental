728a16de41f4f826a90fc3e17847329c685e8604
Carlos Cardo <ccardo29@hotmail.com>
2016-10-18 22:12:32 -0400

    index PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Equipment Rental System</title>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="fragment" content="!">
+	</head>
+	<body>
+		<div style="max-width: 1170px;">
+			<link href="http://technorental.com/script/core/framework/libs/pj/css/pj.bootstrap.min.css" type="text/css" rel="stylesheet" />
+<link href="http://technorental.com/script/index.php?controller=pjFrontEnd&action=pjActionLoadCss" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="http://technorental.com/script/index.php?controller=pjFrontEnd&action=pjActionLoad"></script>
+		</div>
+	</body>
+</html>
