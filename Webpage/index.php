74a851fd71a1c213f6801d0e593ec5d107c0729b
Carlos Cardo <ccardo29@hotmail.com>
2017-10-25 15:29:55 -0400

    testing our tem and maps

--- Webpage/index.php
+++ Webpage/index.php
@@ -194,7 +194,10 @@
     <aside class="callout">
         <div class="text-vertical-center">
             <script type="text/javascript" src="http://technorental.com/script/index.php?controller=pjFrontEnd&action=pjActionLoad"></script>
-            <h1>Vertically Centered Text</h1>
+            
+            
+
+            <h1>Our Team</h1>
         </div>
     </aside>
 
@@ -262,9 +265,9 @@
     <section id="contact" class="map">                                                                                                                                           
         <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
         <br />
-        <small>
+        <!--<small>
             <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a>
-        </small>
+        </small>-->
     </section>
 
 
