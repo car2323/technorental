72571f01daee87adc0f1e1d20f6ec265663b75cf
Carlos Cardo <ccardo29@hotmail.com>
2017-10-25 16:27:25 -0400

    comment mapa

--- Webpage/index.php
+++ Webpage/index.php
@@ -262,12 +262,17 @@
     </aside>
 
     <!-- Map -->
-    <section id="contact" class="map">                                                                                                                                           
+    <section id="contact" class="map">    
+      <!--<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ5a2Xcf622YgRDVC1MDJIeQQ&key=AIzaSyDvPCBptlvZbaDQLc0mIFelm_-ehy7y_9U" allowfullscreen></iframe>
+
+
+
+                                                                                                                                        
         <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
         <br />
         <small>
             <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a>
-        </small>
+        </small>-->
     </section>
 
 
